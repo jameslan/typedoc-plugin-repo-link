4d38e74f317e1dbd8a7a1fe90d4ce0931f18a646
James Lan <jameslan@gmail.com>
2025-04-01 15:47:58 -0700

    Upgrade to melange 5.1 and typedoc 0.27

--- src/index.ml
+++ src/index.ml
@@ -5,14 +5,27 @@ let setup_option app =
     |> Application.get_options
     |> Configuration.Options.addDeclaration "test"
 
+let toolbar (origin_toolbar: DefaultThemeRenderContext.toolbar) (context: DefaultThemeRenderContext.t) (props: Models.Reflection.t PageEvent.t) =
+  let toolbar_element = origin_toolbar context props
+in
+  Js.log(toolbar_element);
+  toolbar_element
+
 let setup_injector application =
   let renderer = Application.get_renderer application
 in
   renderer
     |> Renderer.get_hooks
     |> EventHooks.on
-      "body.end"
-      (fun (h) -> ignore h; JSX.createElement "nothing" Js.null)
+        "body.begin"
+        (fun (context) ->
+          (* let origin_toolbar = DefaultThemeRenderContext.get_toolbar context
+        in *)
+          let new_toolbar = context |> DefaultThemeRenderContext.get_toolbar |> toolbar
+        in
+          DefaultThemeRenderContext.set_toolbar context new_toolbar;
+          JSX.createElement "" Js.null
+        )
 
 let load application =
   setup_option application ;
--- src/typedoc.ml
+++ src/typedoc.ml
@@ -1,43 +1,71 @@
-type any = Any : 'a -> any [@@unboxed]
+type any = Any : 'a -> any [@@ocaml.boxed]
+
 module Configuration = struct
   module Options = struct
     type t
-    external addDeclaration : string -> unit = "addDeclaration" [@@mel.send.pipe: t]
+
+    external addDeclaration : string -> (t[@mel.this]) -> unit = "addDeclaration" [@@mel.send]
   end
 end
+
 module EventHooks = struct
   type ('k, 'r) t
-  external on : string -> ('k -> 'r) -> ?order:int -> unit = "on" [@@mel.send.pipe: ('k, 'r) t]
+  external on : string -> ('k -> 'r) -> ?order:int -> (('k, 'r) t [@mel.this]) -> unit = "on" [@@mel.send]
+end
+
+module JSX = struct
+  module Element = struct
+    type t
+
+    external get_tag : t -> string = "tag" [@@mel.get]
+    external get_props : t -> any Js.dict Js.null = "props" [@@mel.get]
+  end
+
+  type element = Element.t
+
+  external createElement : string -> any Js.dict Js.null -> element = "createElement" [@@mel.scope "JSX"] [@@mel.module "typedoc"]
+end
+
+module Models = struct
+  module Reflection = struct
+    type t
+  end
+end
+
+module PageEvent = struct
+  type 'a t
 end
 
 module DefaultThemeRenderContext = struct
   type t
-end
+  type toolbar = t -> Models.Reflection.t PageEvent.t -> JSX.element
 
-module JSX = struct
-  type element
-  external createElement : string -> any Js.Dict.t Js.Null.t -> element = "createElement" [@@mel.scope "JSX"] [@@mel.module "typedoc"]
+  (* external set_toolbar : t -> toolbar -> unit = "toolbar" [@@mel.set] *)
+  external set_toolbar : t -> ((t -> Models.Reflection.t PageEvent.t -> JSX.element)[@mel.uncurry]) -> unit = "toolbar" [@@mel.set]
+  external get_toolbar : t -> toolbar = "toolbar" [@@mel.get]
 end
 
 module AbstractComponent = struct
   type t
 end
-module ChildableComponent = struct
+
+module ParentalComponent = struct
   include AbstractComponent
   type t = AbstractComponent.t
   (* external get_component : t -> string ->  *)
 end
 
 module Renderer = struct
-  include ChildableComponent
-  type t = ChildableComponent.t
+  include ParentalComponent
+  type t = ParentalComponent.t
+
   external get_hooks : t -> (DefaultThemeRenderContext.t, JSX.element) EventHooks.t = "hooks" [@@mel.get]
 end
 
 module Application = struct
-  include ChildableComponent
-  type t = ChildableComponent.t
+  include ParentalComponent
+  type t = ParentalComponent.t
+
   external get_options : t -> Configuration.Options.t = "options" [@@mel.get]
   external get_renderer : t -> Renderer.t = "renderer" [@@mel.get]
 end
-
